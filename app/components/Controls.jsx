eaf4630ee0cd386bad176af36a2fa2d6834d7339
Stephen Davies <stephen.davies@ixonos.com>
2016-07-22 10:36:20 +0300

    Control handlers

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -7,22 +7,27 @@ module.exports = React.createClass({
     },
 
 
+    onStatusChange(newStatus) {
+        return () => this.props.onStatusChange(newStatus);
+    },
+
     render(){
+
 
         let { countdownStatus } = this.props;
 
         let renderStopStartButton = () => {
             if (countdownStatus === 'started') {
-                return <button className="button secondary">Pause</button>;
+                return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>;
             } else if (countdownStatus === 'paused') {
-                return <button className="button primary">Start</button>;
+                return <button className="button primary"   onClick={this.onStatusChange('started')}>Start</button>;
             }
         };
 
         return (
             <div className="controls">
                 {renderStopStartButton()}
-                <button className="button alert hollow">Clear</button>
+                <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
             </div>
         );
     }
